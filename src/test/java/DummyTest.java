c3b9c3a0aef8b62605f2205e51860958a2ac7949
Rafal Glowacki <rglowacki6@gmail.com>
2015-07-18 14:00:51 +0200

    Added tests

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertThat(true, equalTo(true));
+    }
+}
